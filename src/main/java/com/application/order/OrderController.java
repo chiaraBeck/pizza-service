0938fc41d55ed4cc2da707caa87ab41166067989
chiaraBeck <chiara.beck@sports4me.at>
2023-11-09 16:50:41 +0100

    added controllers

---
+++ src/main/java/com/application/order/OrderController.java
@@ -0,0 +1,8 @@
+package com.application.order;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    
+}
---
+++ src/main/java/com/application/pizza/PizzaController.java
@@ -0,0 +1,7 @@
+package com.application.pizza;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PizzaController {
+}
