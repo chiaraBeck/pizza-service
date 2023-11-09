b2f818022c84e72c754274c1c40ada9469666c14
chiaraBeck <chiara.beck@sports4me.at>
2023-11-09 19:19:38 +0100

    added test call

--- src/main/java/com/application/Application.java
+++ src/main/java/com/application/Application.java
@@ -1,10 +1,23 @@
 package com.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	@GetMapping("/message")
+	public String getMessage(){
+		logger.info("HELLO WORLD");
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
